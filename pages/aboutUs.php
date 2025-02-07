0b4b5c4519038481fec8a3b83d8a7ce703bb199a
rajeshwari16945 <rajeshwari16945@gmail.com>
2025-02-07 12:54:24 -0600

    about changes

--- pages/aboutUs.php
+++ pages/aboutUs.php
@@ -18,20 +18,8 @@
                     <h2>Team Introduction</h2>
                     <div class="team-member">
                         <div class="member-details">
-                            <h3>Sai Teja Liesetty - 700756098</h3>
-                            <p>Description: Sai Teja has completed tasks related to the Agents&Owners, Advertisment, and Budget Planner page. His contributions include integrating features such as a search bar, list display, agent details, user guide and budget Planner. He utilized PHP, HTML, CSS, Javascript and Mysql technologies in his implementation.</p>
-                        </div>
-                    </div>
-                    <div class="team-member">
-                        <div class="member-details">
                             <h3>Ganamukkula Raja Rajeshwari - 700763633</h3>
-                            <p>Description: Rajeshwari has completed tasks related to the homepage, house search page, and favorite page. Her contributions include integrating features such as a search bar, featured listings display, advanced search capabilities, sorting options, and the ability to save listings. She utilized PHP, HTML, CSS, Javascript and Mysql technologies in her implementation.</p>
-                        </div>
-                    </div>
-                    <div class="team-member">
-                        <div class="member-details">
-                            <h3>Harshitha Nellore - 700744038</h3>
-                            <p>Description: Harshitha has completed tasks related to the House Details and Leasing Info pages. Her contributions include integrating features such as a house display, back button, dynamic content inclusion, image gallery, and leasing details. She utilized PHP, HTML, CSS, JavaScript and Mysql technologies in her implementation.</p>
+                            <p>Description: Rajeshwari has completed tasks related to the homepage, house search page, favorite page, Agents&Owners, Advertisment, Budget Planner, House Details and Leasing Info pages. Her contributions include integrating features such as a search bar, featured listings display, advanced search capabilities, sorting options, dynamic content inclusion, and the ability to save listings. She utilized PHP, HTML, CSS, Javascript and Mysql technologies in her implementation.</p>
                         </div>
                     </div>
                 </div>
